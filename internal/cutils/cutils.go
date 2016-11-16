ef71a2a7904afe908fa7fc61e3161d9b8ad167f0
David Hinkes <david.hinkes@gmail.com>
2016-11-15 21:50:40 -0800

    gofmt on cutils package

--- internal/cutils/cutils.go
+++ internal/cutils/cutils.go
@@ -2,17 +2,17 @@
 package cutils
 
 import (
-  //#include <stdlib.h>
-  //#include <strings.h>
-  "C"
-  "unsafe"
+	//#include <stdlib.h>
+	//#include <strings.h>
+	"C"
+	"unsafe"
 )
 
 // Strcpy is just like C's strcpy.
 // Note that we deal with unsafe.Pointer instead of
 // *C.char in the API because *C.char can't be exported.
 func Strcpy(dest unsafe.Pointer, msg string) {
-  ptr := C.CString(msg)
+	ptr := C.CString(msg)
 	defer C.free(unsafe.Pointer(ptr))
 	C.memcpy(dest, unsafe.Pointer(ptr), C.size_t(len(msg))+1)
 }
